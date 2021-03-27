957fe0e5cafbd96717a63928e6e434a8b0ee34f3
Breon Sparrow <beyond.committed@gmail.com>
2021-03-27 10:19:37 -0700

    post and delete, gitignore

--- server.js
+++ server.js
@@ -25,22 +25,42 @@ const noteTracking = (storedNotes) => {
 };
 
 // route paths matching the request to endpoints and transfering the file at the given path and setting a response based on the file extension
-app.get("/assets/css/styles.css", function (req, res) {
+app.get("/assets/css/styles.css", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
 });
 
-app.get("/assets/js/index.js", function (req, res) {
+app.get("/assets/js/index.js", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
 });
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res) {
+app.get("/api/notes", (req, res) => {
   return res.json(storedNotes);
+});
+// POST and DELETE
+app.post("/api/notes", (req, res) => {
+  const writeNote = req.body;
+  const id = storedNotes.length;
+  writeNote.id = id + 1;
+  storedNotes.push(writeNote);
+  noteTracking(storedNotes);
+  return res.json(storedNotes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  delete storedNotes[id - 1];
+  noteTracking(storedNotes);
+  res.send(storedNotes);
+});
+
+app.listen(PORT, () => {
+  console.log("Listening at http://localhost:" + PORT);
 });
